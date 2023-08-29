90dc79760e1d7ff759a27e66b441a256ac4dff3e
Yuminyumin <102581107+Yuminyumin@users.noreply.github.com>
2023-08-29 14:26:34 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+      version = ">= 2.0.0"
+    }
+  }
+}
+
 resource "local_file" "abc" {
   content  = "abc!"
   filename = "${path.module}/abc.txt"
