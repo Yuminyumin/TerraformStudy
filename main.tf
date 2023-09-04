02afc2a72006aa53e0bb53edeb10dde8be15d83f
Yuming <102581107+Yuminyumin@users.noreply.github.com>
2023-09-04 13:26:56 +0900

    .

--- main.tf
+++ main.tf
@@ -1,14 +1,10 @@
 terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    local = {
-      source = "hashicorp/local"
-      version = ">= 2.0.0"
-    }
+  backend "local" {
+    path = "state/terraform.tfstate"    
   }
 }
 
 resource "local_file" "abc" {
-  content  = "abc!"
+  content  = "123456!" # apply 동작 수행을 위해 content 내용을 수정
   filename = "${path.module}/abc.txt"
 }
